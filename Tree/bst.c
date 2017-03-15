8ec3c518376effc821e39aeb1fad9553ddb6c009
manoharthorat <manoharthorat725@gmail.com>
2017-03-15 23:05:56 +0530

    Added tree programs

---
+++ Tree/bst.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node 
+{
+	struct node * left;
+	int data;
+	struct node * right;
+};
+
+struct node * CreateBSt(struct node ** head, int no)
+{
+}
+void Preorder( struct node * head)
+{
+}
+void InOrder(struct node * head)
+{
+}
+void PostOrder(struct node * head)
+{
+}
+int CountNodes(struct node * head)
+{
+
+}
+int CountLeafNodes(struct node * head)
+{
+}
+int CountNonLeafNodes(struct node * head)
+{
+}
+int MirrorBst(struct node *head)
+{
+}
+int CompareBst(struct node * head, struct node * head1)
+{
+}
+
+struct node * CopyBst(struct node * head)
+{
+}
+int DisplayLevel(struct node * head, int no)
+{
+}
+int DisplayHeight(struct node * head, int no)
+{
+}
+
+void InsertNode(struct node * head)
+{
+}
+
+struct node * DeleteNode(struct node * head, int no)
+{
+}
+
+
+
+
+int main()
+{
+	struct node * first=NULL;
+	return 0;
+}
+
