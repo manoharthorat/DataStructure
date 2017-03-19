a754abeeda05654ade2153473b8fb9dd5e809e45
manoharthorat <manoharthorat725@gmail.com>
2017-03-19 17:27:25 +0530

    Added doubly linked list code

--- LinkedList/DoublyLL/dll.c
+++ LinkedList/DoublyLL/dll.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<string.h>
 struct node 
 {
 	struct node * prev;
@@ -10,23 +10,130 @@ struct node
 
 int InsertFirst(struct node ** head, int no)
 {
-	
+	struct node * newnode=(struct node *)malloc(sizeof(struct node ));
+	if(newnode==NULL)
+	{
+		return -1;
+	}
+	memset(newnode,0,sizeof(struct node));
+	newnode->prev=NULL;
+	newnode->data=no;
+	newnode->next=NULL;
+
+	if((*head)==NULL)
+	{
+		(*head)=newnode;
+	}
+	else
+	{
+		newnode-> next= (*head);
+		(*head)=newnode;
+		(*head)->prev=newnode;
+	}
+	return 1;
 }
+
+
 void Display(struct node *head)
 {
+	while(head!=NULL)
+	{
+		printf("%d ",head->data);
+		head=head->next;
+	}
+			
 }
 
 int InsertLast(struct node **head, int no)
 {
+	struct node * newnode= (struct node *)malloc(sizeof(struct node ));
+	struct node * temp=*head;
+	if(newnode==NULL)
+	{
+		return -1;
+	}
+	memset(newnode,0,sizeof(struct node));
+	newnode->prev=NULL;
+	newnode->data=no;
+	newnode->next=NULL;
+	if(*head==NULL)
+	{
+		*head=newnode;
+	}
+	else
+	{
+		while(temp->next!=NULL)
+		{
+			temp=temp->next;
+		}
+		temp->next=newnode;
+		newnode=temp->prev;
+	}
+	return 1;
 }
-int CountNOde(struct node * head)
+int CountNode(struct node * head)
 {
+	int count=0;
+	while(head!=NULL)
+	{
+		count++;
+		head=head->next;
+	}
+	return count;
 }
-Int InsertAtPosition(struct node ** head , int pos, int no)
+int InsertAtPosition(struct node ** head , int pos, int no)
 {
+	int i=0;
+	if(*head==NULL)
+	{
+		return -1;
+	}
+	else if(pos==1)
+	{
+		InsertFirst(head,no);
+	}
+	else if(pos==(CountNode(*head))+1)
+	{
+		InsertLast(head,no);
+	}
+	else if(pos > (CountNode(*head)))
+	{
+		return -1;
+	}
+	else
+	{
+		struct node * temp=*head;
+		struct node * newnode=(struct node *)malloc(sizeof(struct node ));
+		memset(newnode,0,sizeof(struct node));
+		newnode->prev=NULL;
+		newnode->data=no;
+		newnode->next=NULL;
+
+		for(i=1;i<(pos-1);i++)
+		{
+			temp=temp->next;	
+		}
+		newnode->next=temp->next;
+		temp->next->prev=newnode;
+		temp->next=newnode;
+		newnode->prev=temp;
+	}
+	return 1;
 }
-int SearchFirst(struct ndoe * head)
+int SearchFirst(struct node * head,int no)
 {
+	int pos=0;
+	while(head!=NULL)
+	{
+		pos++;
+		if(head->data==no)
+		{
+			return pos;
+			break;
+		}
+		head=head->next;
+	}
+	return -1;
 }
 
 int SearchLast(struct node * head)
@@ -34,6 +141,11 @@ int SearchLast(struct node * head)
 }
 int SearchAllOccurance(struct node * head)
 {
+	int count=0;
+	while(head!=NULL)
+	{
+
+	}
 }
 int DeleteFirst(struct node ** head)
 {
@@ -64,6 +176,16 @@ int ReverseList(struct node * head)
 int main()
 {
 struct node * first=NULL;
-	
+InsertFirst(&first,10);
+InsertFirst(&first,20);
+InsertFirst(&first,30);
+InsertFirst(&first,40);
+InsertLast(&first,25);
+Display(first);
+printf("\n");
+InsertAtPosition(&first,4,35);
+Display(first);
+printf("\nSearch First %d \n",SearchFirst(first,35));
+
 return 0;	
 }
